5cedab41521b1d94ffc6874a4d13b129f96c18dd
segni_abera <segniab69@gmail.com>
2025-04-11 22:38:57 +0300

    Transforms room data for consistent structure
    Maps room data to a standardized structure with default values to ensure consistency. Updates rendering logic to use transformed data.
    
    Improves UI reliability and handles missing properties gracefully.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,32 @@
 import getAllRooms from './actions/getAllRooms';
 import RoomCard from './components/RoomCard';
+
+
+
+
 export default async function Home() {
-
- const rooms: Document = await getAllRooms();
-  return (
-
-   
-       <div>
-           {
-            rooms.map((room) => <RoomCard key= {room.$id} room ={room}/>)
-           }
-        
-       </div>
-  )
+         const rooms = (await getAllRooms()) || [];
+         const transformedRooms = rooms.map((room) => ({
+          ...room,
+          user_id: room.user_id ,
+          name: room.name ,
+          description: room.description ,
+          sqft: room.sqft ,
+          capacity: room.capacity,
+          location: room.location ,
+          address: room.address ,
+          amenities: room.amenities ,
+          availability: room.availability,
+          price_per_hour: room.price_per_hour,
+          image: room.image , // Provide a default image path
+          // Add other default values as needed
+        }));
+      
+        return (
+          <div>
+            {transformedRooms.map((room) => (
+              <RoomCard key={room.$id} room={room} />
+            ))}
+          </div>
+        );
+      }
